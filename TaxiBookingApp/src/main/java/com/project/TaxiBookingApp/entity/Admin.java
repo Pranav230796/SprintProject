3b6d80f3f9c600edbaf5239fe98e704148bbe455
rthirumu <rthirumu@INLEN8520024192.corp.capgemini.com>
2022-09-27 14:59:47 +0530

    Abstract User

--- TaxiBookingApp/src/main/java/com/project/TaxiBookingApp/entity/Admin.java
+++ TaxiBookingApp/src/main/java/com/project/TaxiBookingApp/entity/Admin.java
@@ -8,65 +8,23 @@ public class Admin extends User{
 	
 	@Id
 	private int id;
-	private String username;
-	private String password;
-	private long phoneNo;
-	private String emailId;
 	
 	public Admin() {
 		super();
 	}
 
-	public Admin(String username, String password, long phoneNo, String emailId, int id, String username2,
-			String password2, long phoneNo2, String emailId2) {
-		super();
+	public Admin(String username, String password, long phoneNo, String emailId, int id) {
+		super(username, password, phoneNo, emailId);
 		this.id = id;
-		username = username2;
-		password = password2;
-		phoneNo = phoneNo2;
-		emailId = emailId2;
 	}
 
 	public int getId() {
 		return id;
 	}
 
-	public String getUsername() {
-		return username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public long getPhoneNo() {
-		return phoneNo;
-	}
-
-	public String getEmailId() {
-		return emailId;
-	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public void setPhoneNo(long phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-	
 	
 	
 }
--- TaxiBookingApp/src/main/java/com/project/TaxiBookingApp/entity/User.java
+++ TaxiBookingApp/src/main/java/com/project/TaxiBookingApp/entity/User.java
@@ -1,11 +1,14 @@
 package com.project.TaxiBookingApp.entity;
 
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class User {
 	
-	protected String username;
-	protected String password;
-	protected long phoneNo;
-	protected String emailId;
+	private String username;
+	private String password;
+	private long phoneNo;
+	private String emailId;
 		
 	public User(String username, String password, long phoneNo, String emailId) {
 		super();
@@ -18,5 +21,38 @@ public abstract class User {
 	public User() {
 		super();
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
 	
 }
